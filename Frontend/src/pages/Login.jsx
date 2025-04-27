dffef4f5fd89a91fb845d53ecf349f4654cc1432
Rahal <smarsathsara2002@gmail.com>
2025-04-27 10:19:26 +0530

    starting the Login page

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [state, setState] = useState("sign up");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
-export default Login
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form
+      action=""
+      className="min-h-[80vh] shadow-lg rounded-lg flex items-center"
+    >
+      <div>
+        <p>{state === "sign up" ? "Create Account" : "Login"}</p>
+        <p>
+          Please {state === "sign up" ? "sign up" : "login"} to book appointment
+        </p>
+      </div>
+    </form>
+  );
+};
+
+export default Login;
