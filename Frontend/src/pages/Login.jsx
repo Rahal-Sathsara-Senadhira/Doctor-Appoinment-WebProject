7eb9f53f93a0db5d25923006822926cb576efbc7
Rahal <smarsathsara2002@gmail.com>
2025-04-27 23:54:16 +0530

    finished My profile frontend

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -11,35 +11,74 @@ const Login = () => {
   };
 
   return (
-    <form
-      action=""
-      className="min-h-[80vh] flex items-center"
-    >
+    <form action="" className="min-h-[80vh] flex items-center">
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-sm text-zinc-600 shadow-lg">
-        <p className="text-2xl font-semibold">{state === "sign up" ? "Create Account" : "Login"}</p>
+        <p className="text-2xl font-semibold">
+          {state === "sign up" ? "Create Account" : "Login"}
+        </p>
         <p>
           Please {state === "sign up" ? "sign up" : "login"} to book appointment
         </p>
-        {
-          state ==='sign up'?<div className="w-full">
-          <p>User Name</p>
-          <input type="text" onChange={(e)=>setName(e.target.name)} value={name} required className="border border-zinc-300 rounded w-full p-2 mt-1"/>
-        </div>:<div></div>
-        }
-        
+        {state === "sign up" ? (
+          <div className="w-full">
+            <p>User Name</p>
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+              className="border border-zinc-300 rounded w-full p-2 mt-1"
+            />
+          </div>
+        ) : (
+          <div></div>
+        )}
+
         <div className="w-full">
           <p>Email</p>
-          <input type="email" onChange={(e)=>setEmail(e.target.name)} value={email} required className="border border-zinc-300 rounded w-full p-2 mt-1"/>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+          />
         </div>
         <div className="w-full">
           <p>Password</p>
-          <input type="password" onChange={(e)=>setPassword(e.target.name)} value={password} required className="border border-zinc-300 rounded w-full p-2 mt-1"/>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+            className="border border-zinc-300 rounded w-full p-2 mt-1"
+          />
         </div>
-        <button className="bg-primary text-white text-base w-full py-2 rounded-md">{state === 'sign up'?"Create Account":"Login"}</button>
-      
-      {
-        state ==='sign up'?<p>Already have an account? <span onClick={()=>setState("login")} className="underline text-primary cursor-pointer">Login here</span></p>:<p>Create an new account? <span onClick={()=>setState('sign up')} className="underline text-primary cursor-pointer">Click here</span></p>
-      }
+        <button className="bg-primary text-white text-base w-full py-2 rounded-md">
+          {state === "sign up" ? "Create Account" : "Login"}
+        </button>
+
+        {state === "sign up" ? (
+          <p>
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("login")}
+              className="underline text-primary cursor-pointer"
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create an new account?{" "}
+            <span
+              onClick={() => setState("sign up")}
+              className="underline text-primary cursor-pointer"
+            >
+              Click here
+            </span>
+          </p>
+        )}
       </div>
     </form>
   );
--- Frontend/src/pages/MyProfile.jsx
+++ Frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const MyProfile = () => {
-  return (
-    <div>MyProfile</div>
-  )
-}
+  const [userData, setUserData] = useState({
+    name: "Rahal Sathsara",
+    image: assets.profile_pic,
+    email: "smarsathsara@gmail.com",
+    phone: "+94 74 342 9610",
+    address: {
+      line1: "636/D",
+      line2: "Sama MW",
+      line3: "Ragama",
+    },
+    gender: "Male",
+    dob: "2002-11-14",
+  });
 
-export default MyProfile
+  const [isEdit, setIsEdit] = useState(false);
+  const hadleChanged = (e) => {
+    const { name, value } = e.target;
+    // Check if it's an address field
+    if (name.startsWith("address.")) {
+      const field = name.split(".")[1]; // Get 'line1', 'line2', or 'line3'
+      setUserData((prev) => ({
+        ...prev,
+        address: { ...prev.address, [field]: value },
+      }));
+    } else {
+      setUserData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
+  };
+
+  return (
+    <div className="max-w-lg flex flex-col gap-2 text-sm">
+      <img  src={userData.image} alt="" className="w-36 rounded"/>
+      {isEdit ? (
+        <input
+          type="text"
+          name="name"
+          value={userData.name}
+          onChange={hadleChanged}
+          className="bg-gray-50 text-3xl font-medium max-w-60 mt-4"
+        />
+      ) : (
+        <p>{userData.name}</p>
+      )}
+      <hr />
+      <div>
+        <p>CONTACT INFORMATION</p>
+        <div>
+          <p>Email :</p>
+          {isEdit ? (
+            <input
+              type="text"
+              name="email"
+              onChange={hadleChanged}
+              value={userData.email}
+            />
+          ) : (
+            <p>{userData.email}</p>
+          )}
+
+          <p>Phone :</p>
+          {isEdit ? (
+            <input
+              type="text"
+              name="phone"
+              onChange={hadleChanged}
+              value={userData.phone}
+            />
+          ) : (
+            <p>{userData.phone}</p>
+          )}
+
+          <p>Address :</p>
+          {isEdit ? (
+            <p>
+              <input
+                type="text"
+                name="address.line1"
+                onChange={hadleChanged}
+                value={userData.address.line1}
+              />
+              <br />
+              <input
+                type="text"
+                name="address.line2"
+                onChange={hadleChanged}
+                value={userData.address.line2}
+              />
+              <br />
+              <input
+                type="text"
+                name="address.line3"
+                onChange={hadleChanged}
+                value={userData.address.line3}
+              />
+            </p>
+          ) : (
+            <p>
+              <p>{userData.address.line1},</p>
+              <p>{userData.address.line2},</p>
+              <p>{userData.address.line3}</p>
+            </p>
+          )}
+        </div>
+      </div>
+      <div>
+        <p>BASIC INFORMATION</p>
+        <div>
+          <p>Gender :</p>
+          {isEdit ? (
+            <select
+              name="gender"
+              id=""
+              value={userData.gender}
+              onChange={hadleChanged}
+            >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+            </select>
+          ) : (
+            <p>{userData.gender}</p>
+          )}
+          <p>Date of Birth :</p>
+          {isEdit ? (
+            <input type="date" name="dob" id="" value={userData.dob} onChange={hadleChanged}/>
+          ) : (
+            <p>{userData.dob}</p>
+          )}
+        </div>
+      </div>
+      {isEdit ? (
+        <button
+          onClick={() => setIsEdit(false)}
+          className="border border-zinc-600 text-sm p-2 px-4 rounded-full"
+        >
+          Save Information
+        </button>
+      ) : (
+        <button
+          onClick={() => setIsEdit(true)}
+          className="border border-zinc-600 text-sm p-2 px-4 rounded-full"
+        >
+          Edit
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default MyProfile;
